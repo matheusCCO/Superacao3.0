7da223c2bd8be68ba4dd8a1a41b61517ac9a2334
Matheus <matheus281.1@hotmail.com>
2024-04-21 17:47:47 -0300

    Tela de login criada, add fotos do superação antigo

---
+++ Login/index.php
@@ -0,0 +1,26 @@
+<?php // require_once "function.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styleLogin.css">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+    
+    <div class="divLogin">
+    <h1>SUPERAÇÃO 3.0</h1>
+        
+        <form method="POST">
+           
+            <input type="email" name="email" placeholder="E-mail">
+            <input type="password" name="senha" placeholder="Senha">
+            <input type="submit" name="logar" value="Login">
+        </form>
+        <?php // login($connect)?>
+    </div>
+    </main>
+</body>
+</html>
