76ba16bbb8f72d0ff0b4f021a8a76bc89b6ea6d9
matheuscco <matheus281.1@hotmail.com>
2024-04-26 17:35:42 -0300

    criando a pagina de home

---
+++ home/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styleHome.css">
+    <title>Superação3.0</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a class="logo" href="#"></a>Escolher meus avaliadores</li>
+                <li><a class="logo" href="#"></a>Realizar Feedback</li>
+                <li><a class="logo" href="#"></a>Feedback não solicitado</li>
+            </ul>
+
+        </nav>
+    </header>
+    <main></main>
+</body>
+</html>
